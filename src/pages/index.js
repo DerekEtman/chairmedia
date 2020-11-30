5c072f765721decb0500f4ed5784c2b020def634
Derek Etman <dereketman@gmail.com>
2020-11-30 15:02:10 -0800

    init: Setting up Netlify CMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
+import { Link } from "gatsby"
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Link to="/about/">About</Link>
+    </div>
+  
+    )
 }
